71383dfee4d1009adfc17089c70fa4815d7aeff3
zzype <zhangzhongyuan@sinovatech.com>
2022-03-12 14:59:23 +0800

    String 的 replace方法的用法

--- src/com/demo/zzy/SubStringTest.java
+++ src/com/demo/zzy/SubStringTest.java
@@ -12,5 +12,8 @@ public class SubStringTest {
         //substring,前闭后合，返回截取后的字符串
         System.out.println(str.substring(1, 3));
 
+        //replace 返回替代后的新字符串
+        String str2 = "1 2 3 ";
+        System.out.println(str2.replace(" ", ""));
     }
 }
