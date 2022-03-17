b6d77d0c6c78d41643f17a37360ad117d44934eb
zzype <zhangzhongyuan@sinovatech.com>
2022-03-17 09:15:02 +0800

    查不到数据返回null

---
+++ src/com/demo/zzy/note/WorkShouldKnow.java
@@ -0,0 +1,13 @@
+package com.demo.zzy.note;
+
+/**
+ * @author 张忠源
+ * @description WorkShouldKnow
+ * @since 2022/3/17 9:11
+ */
+public class WorkShouldKnow {
+
+    /**
+     * 普通查询返回值,如果查询不到,对象返回null,集合返回空的集合,使用的时候,需要判断,并且提前返回
+     */
+}
