ad82a5bc75ec1cbb82c997e967b43de493456e3e
张忠源 <zhangzhongyuan@sinovatech.com>
2022-03-24 20:38:16 +0800

    http协议

---
+++ standard-service/src/main/java/controller/TestController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import com.oracle.webservices.internal.api.databinding.DatabindingMode;
+
+/**
+ * @author 张忠源
+ * @description TestController
+ * @since 2022/3/24 20:30
+ */
+//@RequestMapping(value = "/test")
+    //@RestController
+public class TestController {
+
+
+    //@RequestMapping(value = "/test", method = RequestMethod.POST)
+
+   /* public String  test(@RequestParam("name") String name,@RequestBody String body){
+
+
+
+
+       return "test";
+    }
+    */
+
+    /**
+     * @RequestParam("name") String name,是拼接在url后面的参数，postman中是params
+     * @RequestBody String body,是在body中的参数，postman中是body,row
+     * contextType="application/json",是指定传输的格式，postman中是content-type
+     */
+}
