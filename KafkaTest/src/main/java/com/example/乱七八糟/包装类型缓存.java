53dbcb73ad32375397c6a45f1a0b04243c7bd550
张忠源 <zhangzhongyuan@sinovatech.com>
2022-08-09 13:59:49 +0800

    int  包装类型127以下存在虚拟机栈中

--- KafkaTest/src/main/java/com/example/乱七八糟/包装类型缓存.java
+++ KafkaTest/src/main/java/com/example/乱七八糟/包装类型缓存.java
@@ -1,5 +1,7 @@
 package com.example.乱七八糟;
 
+import java.util.HashMap;
+
 /**
  * @author zhangzhongyuan@sinovatech.com
  * @description
@@ -11,13 +13,19 @@ public class 包装类型缓存 {
 
         /**
          * int 127以下会缓存在虚拟机栈当中,所以==  比较地址是一致的,但是以上的数据就是存在堆中,地址就不同
+         *
+         *
          */
         Integer i1 = 127;
         Integer i2 = 127;
         Integer i3 = 200;
         Integer i4 = 200;
-        System.out.println(i1==i2);
-        System.out.println(i3==i4);
+        System.out.println(i1 == i2);  //输出true
+        System.out.println(i3 == i4);  //输出false
 
+       /* new HashMap<>();
+
+        int i = 4 >> 1;
+        System.out.println(i);*/
     }
 }
