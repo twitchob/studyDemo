1fdaa0055c0d5aa2b48a4edb43eebbdf7f1e1189
zzypersonally@gmail.com <zzypersonally@gmail.com>
2022-08-19 14:59:32 +0800

    vertx  demo

---
+++ vertxTest/src/test/java/com/zzy/vertxtest/VertxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzy.vertxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VertxTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
