07c53c8d9d371af4399ee9fdce929194dd96d851
zzypersonally@gmail.com <zzypersonally@gmail.com>
2022-08-24 16:50:05 +0800

    打印 syn 和 ack 字符

---
+++ JavaTest/src/main/java/com/example/javatest/temp/ASCII_SYN_ACK.java
@@ -0,0 +1,17 @@
+package com.example.javatest.temp;
+
+/**
+ * @author zhangzhongyuan@szanfu.cn
+ * @description
+ * @since 2022/8/24 16:45
+ */
+public class ASCII_SYN_ACK {
+    public static void main(String[] args) {
+        char chr = 65;
+        System.out.println(chr);
+        char syn = 22;
+        char ack = 6;
+        System.out.println(syn);
+        System.out.println(ack);
+    }
+}
