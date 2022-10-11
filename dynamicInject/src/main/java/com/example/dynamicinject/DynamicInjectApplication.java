11be5a31c66c5ce893a46a4f1361f284f15028da
zzypersonally@gmail.com <zzypersonally@gmail.com>
2022-10-11 21:02:43 +0800

    druid 连接池

---
+++ dynamicInject/src/main/java/com/example/dynamicinject/DynamicInjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynamicinject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicInjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicInjectApplication.class, args);
+    }
+
+}
---
+++ dynamicInject/src/test/java/com/example/dynamicinject/DynamicInjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamicinject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicInjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
